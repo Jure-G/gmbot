a948464fa514bb8537e259278a673aad4db40e23
Jure-G <gorjup.jure91@gmail.com>
2026-05-13 11:51:25 +0200

    Completely change the way this program works, remove classes, use dictionaries instead, update README, use regex instead of if in statements. Import data now returns a list of npc dictionaries where npc contains name, conditions, max and current hp, initiative bonus, spellslots, rechargable abilities and the whole statblock formatted for easier reading.

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
 This is a combat helper for game masters in DND 5e.
--   It tracks monster stats (stat block + current hp, current initiative, current spell slots, any condition it is under and 
-    any special resources it may have)
+-   It tracks monster stats (stat block + current hp, current initiative, current spell slots, any condition it is under and any rechargable abilities it may have)
 -   It constructs initiative order and gives the gm the currently active character and it's stats.
 -   It rolls some things automatically and asks for gm imput when necessary.
 
-When combat starts it asks how many players there are, their names and their current initiative, imports NPC stats from a file, rolls inititative 
-for them and constructs initiative order. 
+When combat starts it asks how many players there are, their names and their current initiative, imports NPC stats from a file, rolls inititative for them and constructs the initiative order. 
 
 Then it presents the first character in the initiative order, prints it's stats and asks you what to do.
-options are MOVE, ATTACK, CAST SPELL, USE SPECIAL, ADD/REMOVE CONDITION, END TURN and REMOVE FROM FIGHT.
-if it is a player it only prints the player name and conditions the player is under. The only options are ADD/REMOVE CONDITION, END TURN and
-REMOVE FROM FIGHT.
+options are ATTACK, CAST SPELL, USE RECHARGABLE, ADD/REMOVE CONDITION, END TURN, REMOVE FROM FIGHT and ROLL.
+if it is a player it only prints the player name and conditions the player is under. The only options are ATTACK, ADD/REMOVE CONDITION, END TURN, REMOVE FROM FIGHT and ROLL.
 
-whatever you choose to do, the program will do any rolls necessary, print them out and print out an updated stat block with removed resources.
-if you END TURN or REMOVE FROM FIGHT the program goes to the next character in initiative order and do the same.
+- ATTACK x reduces hp of the target(x) by a specified amount
+- CAST SPELL reduces the amount of spell slots
+- USE RECHARGABLE uses rechargable and flags it as used
+- ADD/REMOVE CONDITION adds or removes condition you specify for amount of turns you specify
+- END TURN goes to the next character in initiative
+- REMOVE FROM FIGHT removes the character from initiative order and goes to the next one
+- ROLL xdx (where x is any positive intiger) + or - y (where y is any intiger) simulates rolling x times dx tice and adding and subtracting y ex. 2d6+3 (adds two random numbers from 1 to 6 (including both) plus 3)
 
 statblocks.txt is an example of how statblocks must be written to get imported properly.
 before every statblock there must be a num = *number of same stat blocks you want included*
--- import_data.py
+++ import_data.py
@@ -1,35 +1,86 @@
-#Imports statblocks and returns a list of dictionaries containing everything you need to create an appropriate amount of NPC objects
-#name, initiative, max_hp, speed, statblock
+#Imports statblocks and returns a list of dictionaries containing npcs
+import re
 
 def import_data(path_to_file):
     with open(path_to_file) as f:
         statblocks = f.read()
     list_of_lines = statblocks.splitlines()
-    
+
     #list of dictionaries that contain (index of statblock start, index of statblock end, number after "num =") 
     list_of_statblock_data = []
 
     line_i = 0
     for line in list_of_lines:
-        if "num =" in line:
-            words = line.split()
-            list_of_statblock_data.append({"start": line_i+1, "end": len(list_of_lines), "num": words[2]})
+        pattern = re.compile(r"num \= (\d+)")
+        match = pattern.search(line)
+        if match:
+            list_of_statblock_data.append({"start": line_i+1, "end": len(list_of_lines), "num": int(match.group(1))})
         line_i += 1
 
-    #correct the end point in statblocks
-    statblock_i = 0
+    #set the end point in statblocks
+    statblock_i = 1
+    while statblock_i < len(list_of_statblock_data):
+        list_of_statblock_data[statblock_i-1]["end"] = list_of_statblock_data[statblock_i]["start"] - 1
+        statblock_i += 1
+
+    statblock_list = []
     for statblock_data in list_of_statblock_data:
-        if statblock_i == 0:
-            statblock_i = 1
-        else:
-            list_of_statblock_data[statblock_i-1]["end"] = list_of_statblock_data[statblock_i]["start"] - 2
-            statblock_i += 1
+        i = 0
+        while i < statblock_data["num"]:
+            statblock_list.append(list_of_lines[statblock_data["start"]:statblock_data["end"]])
+            i += 1
     
+    list_npcs = []
 
+    for statblock in statblock_list:
+        npc = {}
+        #set name
+        npc["name"] = statblock[0]
+        spell_slots = {}
+        rechargable = {}
+        #set empty list of conditions
+        npc["conditions"] = []
+        formatted_statblock = []
+        for line in statblock:
+            #set rechargable
+            pattern = re.compile(r"^\s*(.+)\s+\(Recharge (\d+-\d+)\)")
+            match = pattern.search(line)
+            if match:
+                rechargable[match.group(1)] = match.group(2)
 
+            #set spell_slots
+            pattern = re.compile(r"(\d[st|nd|rd|th]+) level \((\d) slot")
+            match = pattern.search(line)
+            if match:
+                spell_slots[match.group(1)] = int(match.group(2))
 
-      
+            #set max_hp and current_hp
+            pattern = re.compile(r"Hit Points (\d+) \(\d+d\d+")
+            match = pattern.search(line)
+            if match:
+                npc["max_hp"] = match.group(1)
+                npc["current_hp"] = match.group(1)
 
-
-#only for testing, remove after done
+            #set formatted statblock(list of lines) and initiative
+            pattern = re.compile(r"STR\s+DEX\s+CON\s+INT\s+WIS\s+CHA")
+            match = pattern.search(line)
+            pattern_stats = re.compile(r"(\d+\s+\([+-]\d+\))\s+(\d+\s+\([+-]\d+\))\s+(\d+\s+\([+-]\d+\))\s+(\d+\s+\([+-]\d+\))\s+(\d+\s+\([+-]\d+\))\s+(\d+\s+\([+-]\d+\))")
+            match_stats = pattern_stats.search(line)
+            if match:
+                continue
+            elif match_stats:
+                formatted_line = "STR = " + match_stats.group(1) + " DEX = " + match_stats.group(2) + " CON = " + match_stats.group(3) + " INT = " + match_stats.group(4) + " WIS = " + match_stats.group(5) + " CHA = " + match_stats.group(6)
+                formatted_statblock.append(formatted_line)
+                pattern_initiative = re.compile(r"\d+\s\(([+-]\d+)\)")
+                match_initiative = pattern_initiative.search(match_stats.group(2))
+                if match_initiative:
+                        npc["initiative_bonus"] = match_initiative.group(1)
+            else:
+                formatted_statblock.append(line)
+        npc["formatted_statblock"] = formatted_statblock
+        npc["spell_slots"] = spell_slots
+        npc["rechargable"] = rechargable
+        list_npcs.append(npc)
+    return list_npcs
 import_data("./statblocks.txt")
+
